7653247d855f39caa93720d756cd5ca726a7c2cf
Kadinata <2396847+Kadinata@users.noreply.github.com>
2023-10-16 10:39:54 -0500

    Adding heartbeat and its API endpoint handler

---
+++ src/routes/heartbeat/routes.js
@@ -0,0 +1,23 @@
+//===========================================================================
+//  
+//===========================================================================
+const express = require('express');
+const { validateHandler } = require('../../modules/endpoint_handler');
+const protectedRoute = require('../../modules/auth/protected_route');
+
+const validateHeartbeatRequest = validateHandler((req) => {
+  if (!!req.heartbeat) return;
+  const message = 'Heartbeat service has not been initialized';
+  throw new Error(message);
+});
+
+const initialize = () => {
+  const router = express.Router();
+  router.use(protectedRoute, validateHeartbeatRequest);
+  router.get('/', (req, res, next) => req.heartbeat.handleRequest(req, res, next));
+
+  return router;
+};
+
+module.exports = { initialize };
+//===========================================================================
