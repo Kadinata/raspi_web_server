bcb17a1500c809e918d77db198a8858b72307b53
Kadinata <2396847+Kadinata@users.noreply.github.com>
2024-08-20 15:04:09 -0700

    Adding JWT authentication middleware to auth middleware

--- __tests__/middlewares/auth.test.js
+++ __tests__/middlewares/auth.test.js
@@ -1,16 +1,19 @@
 //===========================================================================
 //  
 //===========================================================================
+const passport = require('passport');
 const auth_middleware = require('../../src/middlewares/auth');
 const Auth = require('../../src/modules/auth/auth');
 const { User } = require('../../src/models');
 const jwtsm = require('../../src/modules/jwt/jwt_secret_manager');
-const passport = require('passport');
 const authModule = require('../../src/modules/auth');
+const mock_req_res_next = require('../__utils__/mock_req_res_next');
+const jwtAuthentication = require('../../src/middlewares/auth/jwt_authentication');
 
 const MOCK_JWT_SECRET = 'Test JWT Secret';
 const MOCK_JWT_SECRET_FILE = '/path/to/jwt/secret';
 const MOCK_DB_FILE = '/path/to/db/file';
+const MOCK_USER = { username: 'someuser' };
 
 jest.mock('../../src/modules/jwt/jwt_secret_manager', () => ({
   load_or_create: jest.fn(async (jwt_secret_file) => MOCK_JWT_SECRET),
@@ -18,6 +21,11 @@ jest.mock('../../src/modules/jwt/jwt_secret_manager', () => ({
 
 jest.mock('../../src/modules/auth/auth_passport_config', () => ({
   configure: jest.fn(),
+}));
+
+jest.mock('../../src/middlewares/auth/jwt_authentication.js', () => jest.fn((req, res, next) => {
+  req.user = MOCK_USER;
+  next();
 }));
 
 beforeEach(() => {
@@ -54,25 +62,53 @@ describe('Auth Middleware', () => {
       expect(mock_passport_initialize).toHaveBeenCalledTimes(1);
     });
 
-    it('should return a provider function that attaches the auth instance to the req object', async () => {
-      const mock_passport_initialize = jest.spyOn(passport, 'initialize');
-      const req = {};
-      const res = {};
-      const next = jest.fn();
+    it('should return a middleware that is a function', async () => {
+      const provider = await auth_middleware.initialize(MOCK_JWT_SECRET_FILE, MOCK_DB_FILE);
+      expect(typeof provider).toEqual('function');
+    });
 
+    it('should return a middleware that attaches the auth instance to the req object', async () => {
+      const { req, res, next } = mock_req_res_next();
       const provider = await auth_middleware.initialize(MOCK_JWT_SECRET_FILE, MOCK_DB_FILE);
 
-      expect(typeof provider).toEqual('function');
-      expect(next).toHaveBeenCalledTimes(0);
-      expect(req.login).toBeUndefined();
-      expect(req.logIn).toBeUndefined();
       expect(req.auth).toBeUndefined();
 
       provider(req, res, next);
-      expect(next).toHaveBeenCalledTimes(1);
+      expect(req.auth instanceof Auth).toEqual(true);
+    });
+
+    it("should return a middleware that attaches passport's login functions to the req object", async () => {
+      const { req, res, next } = mock_req_res_next();
+      const provider = await auth_middleware.initialize(MOCK_JWT_SECRET_FILE, MOCK_DB_FILE);
+
+      expect(req.login).toBeUndefined();
+      expect(req.logIn).toBeUndefined();
+
+      provider(req, res, next);
       expect(typeof req.login).toEqual('function');
       expect(typeof req.logIn).toEqual('function');
-      expect(req.auth instanceof Auth).toEqual(true);
+    });
+
+    it("should perform JWT authentication and attach the current user object to the req object", async () => {
+      const { req, res, next } = mock_req_res_next();
+      const provider = await auth_middleware.initialize(MOCK_JWT_SECRET_FILE, MOCK_DB_FILE);
+
+      expect(req.user).toBeUndefined();
+      expect(jwtAuthentication).toHaveBeenCalledTimes(0);
+
+      provider(req, res, next);
+      expect(req.user).toEqual(expect.objectContaining(MOCK_USER));
+      expect(jwtAuthentication).toHaveBeenCalledTimes(1);
+    });
+
+    it("should eventually call the next middleware in the stack", async () => {
+      const { req, res, next } = mock_req_res_next();
+      const provider = await auth_middleware.initialize(MOCK_JWT_SECRET_FILE, MOCK_DB_FILE);
+
+      expect(next).toHaveBeenCalledTimes(0);
+
+      provider(req, res, next);
+      expect(next).toHaveBeenCalledTimes(1);
     });
   });
 });
--- src/middlewares/auth/auth.js
+++ src/middlewares/auth/auth.js
@@ -5,16 +5,16 @@ const passport = require('passport');
 const jwtsm = require('../../modules/jwt/jwt_secret_manager');
 const { User } = require('../../models');
 const authModule = require('../../modules/auth');
+const jwtAuthentication = require('./jwt_authentication');
 
 const initialize = async (JWT_SECRET_FILE) => {
-
   const jwt_secret = await jwtsm.load_or_create(JWT_SECRET_FILE);
   const auth = authModule.initialize(User, jwt_secret);
   const passportHandler = passport.initialize();
 
   const provider = (req, res, next) => {
     req.auth = auth;
-    passportHandler(req, res, next);
+    passportHandler(req, res, () => jwtAuthentication(req, res, next));
   };
 
   return provider;
