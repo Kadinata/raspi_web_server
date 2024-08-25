23d1a852d53ebf609ccfef7df7b5c4d06e6d168f
Kadinata <2396847+Kadinata@users.noreply.github.com>
2024-08-25 12:35:47 -0700

    Updating sysinfo middleware module

--- __tests__/middlewares/sysinfo.test.js
+++ __tests__/middlewares/sysinfo.test.js
@@ -8,90 +8,118 @@ const mock_req_res_next = require('../__utils__/mock_req_res_next');
 
 const mock_sysinfo_stream_start = jest.fn();
 const mock_sysinfo_stream_stop = jest.fn();
+const mock_sysinfo_stream_subscribe = jest.fn();
 
 jest.mock('../../src/modules/sysinfo', () => ({
-  initialize: jest.fn((callback) => ({
-    get_callback: () => callback,
-    stream: {
-      start: mock_sysinfo_stream_start,
-      stop: mock_sysinfo_stream_stop,
-    },
-  })),
+  initialize: jest.fn(() => {
+    const generate = () => {
+      let callback = null;
+      return {
+        get_callback: () => callback,
+        stream: {
+          start: mock_sysinfo_stream_start,
+          stop: mock_sysinfo_stream_stop,
+          subscribe(cb) {
+            mock_sysinfo_stream_subscribe(cb);
+            callback = cb;
+          },
+        },
+      };
+    };
+    return generate();
+  }),
 }));
 
-describe('SysInfo Middleware Tests', () => {
+describe('SysInfo Middleware', () => {
+
+  let sse_handler = null;
 
   beforeEach(() => {
+    sse_handler = sse.Handler('Test Sysinfo');
     jest.clearAllMocks();
   });
 
-  it('should initialize the middleware correctly', () => {
-    const sse_handler = sse.Handler('Test Sysinfo');
+  describe('initialization', () => {
+    it('should return a middleware function', () => {
+      const provider = sysinfo_middleware.initialize(sse_handler);
+      expect(typeof provider).toEqual('function');
+    });
 
-    const mock_sse_client_count_change = jest.spyOn(sse_handler, 'onClientCountChange')
-      .mockImplementation(() => null);
+    it('should initialize the sysinfo module', () => {
+      sysinfo_middleware.initialize(sse_handler);
+      expect(sysinfoModule.initialize).toHaveBeenCalledTimes(1);
+    });
 
-    /** Verify the module is returning a middleware function */
-    const provider = sysinfo_middleware.initialize(sse_handler);
-    expect(sysinfoModule.initialize).toHaveBeenCalledTimes(1);
-    expect(typeof provider).toEqual('function');
+    it('should add a subscriber to the sysinfo stream', () => {
+      sysinfo_middleware.initialize(sse_handler);
+      expect(mock_sysinfo_stream_subscribe).toHaveBeenCalledTimes(1);
+      expect(typeof mock_sysinfo_stream_subscribe.mock.calls[0][0]).toEqual('function');
+    });
 
-    /** Verify the initialization also listens to the sse handler's client count change */
-    expect(mock_sse_client_count_change).toHaveBeenCalledTimes(1);
-    expect(mock_sse_client_count_change.mock.calls[0][0]).toEqual('sysinfo');
-    expect(typeof mock_sse_client_count_change.mock.calls[0][1]).toEqual('function');
+    it('should add a listener to the SSE handler client count change', () => {
+      const mock_sse_client_count_change = jest.spyOn(sse_handler, 'onClientCountChange')
+        .mockImplementation(() => null);
+
+      sysinfo_middleware.initialize(sse_handler);
+      expect(mock_sse_client_count_change).toHaveBeenCalledTimes(1);
+      expect(mock_sse_client_count_change.mock.calls[0][0]).toEqual('sysinfo');
+      expect(typeof mock_sse_client_count_change.mock.calls[0][1]).toEqual('function');
+    });
   });
 
-  it('should attach a sysinfo instance to the req object', () => {
-    const sse_handler = sse.Handler('Test Sysinfo');
+  describe('middleware function', () => {
+    it('should attach a sysinfo instance to the req object', () => {
+      const { req, res, next } = mock_req_res_next();
+      const provider = sysinfo_middleware.initialize(sse_handler);
+      expect(req.sysinfo).toBeUndefined();
 
-    const { req, res, next } = mock_req_res_next();
-    req.sse_handler = sse_handler;
+      provider(req, res, next);
+      expect(req.sysinfo).toBeDefined();
+    });
 
-    const provider = sysinfo_middleware.initialize(sse_handler);
+    it('should call next to invoke the next middleware in the stack', () => {
+      const { req, res, next } = mock_req_res_next();
+      const provider = sysinfo_middleware.initialize(sse_handler);
+      expect(next).toHaveBeenCalledTimes(0);
 
-    expect(req.sysinfo).toBeUndefined();
-    expect(next).toHaveBeenCalledTimes(0);
-
-    /** Invoke the middleware and verify sysinfo object is attached to req */
-    provider(req, res, next);
-    expect(req.sysinfo).not.toBeUndefined();
-    expect(next).toHaveBeenCalledTimes(1);
-
-    const mock_sse_send = jest.spyOn(sse_handler, 'send');
-    expect(mock_sse_send).toHaveBeenCalledTimes(0);
-    expect(typeof req.sysinfo.get_callback()).toEqual('function');
-
-    /** Emit a sysinfo stream data and verify the data is published to the sse handler */
-    req.sysinfo.get_callback()(0xBADDF00D);
-    expect(mock_sse_send).toHaveBeenCalledTimes(1);
-    expect(mock_sse_send).toHaveBeenCalledWith('sysinfo', 0xBADDF00D);
+      provider(req, res, next);
+      expect(next).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should start sysinfo stream if there is a client and stop it if there is no client', () => {
-    const sse_handler = sse.Handler('Test Sysinfo');
+  describe('event handling', () => {
+    it('should forward values emitted from the sysinfo stream to the SSE handler', () => {
+      const { req, res, next } = mock_req_res_next();
+      const mock_sse_send = jest.spyOn(sse_handler, 'send');
+      const provider = sysinfo_middleware.initialize(sse_handler);
 
-    const { req, res, next } = mock_req_res_next();
-    req.sse_handler = sse_handler;
+      provider(req, res, next);
 
-    const provider = sysinfo_middleware.initialize(sse_handler);
-    expect(req.sysinfo).toBeUndefined();
-    expect(next).toHaveBeenCalledTimes(0);
+      expect(mock_sse_send).toHaveBeenCalledTimes(0);
+      expect(typeof req.sysinfo.get_callback()).toEqual('function');
 
-    provider(req, res, next);
-    expect(req.sysinfo).toBeDefined();
-    expect(next).toHaveBeenCalledTimes(1);
+      req.sysinfo.get_callback()(0xBADDF00D);
+      expect(mock_sse_send).toHaveBeenCalledTimes(1);
+      expect(mock_sse_send).toHaveBeenCalledWith('sysinfo', 0xBADDF00D);
+    });
 
-    expect(mock_sysinfo_stream_start).toHaveBeenCalledTimes(0);
-    expect(mock_sysinfo_stream_stop).toHaveBeenCalledTimes(0);
+    it('should start sysinfo stream if the client count is at least 1', () => {
+      sysinfo_middleware.initialize(sse_handler);
+      expect(mock_sysinfo_stream_start).toHaveBeenCalledTimes(0);
 
-    sse_handler.emit('sysinfo/clientChange', 1);
-    expect(mock_sysinfo_stream_start).toHaveBeenCalledTimes(1);
-    expect(mock_sysinfo_stream_stop).toHaveBeenCalledTimes(0);
+      sse_handler.emit('sysinfo/clientChange', 1);
+      expect(mock_sysinfo_stream_start).toHaveBeenCalledTimes(1);
+    });
 
-    sse_handler.emit('sysinfo/clientChange', 0);
-    expect(mock_sysinfo_stream_start).toHaveBeenCalledTimes(1);
-    expect(mock_sysinfo_stream_stop).toHaveBeenCalledTimes(1);
+    it('should start sysinfo stream if there is no client', () => {
+      sysinfo_middleware.initialize(sse_handler);
+
+      sse_handler.emit('sysinfo/clientChange', 1);
+      expect(mock_sysinfo_stream_stop).toHaveBeenCalledTimes(0);
+
+      sse_handler.emit('sysinfo/clientChange', 0);
+      expect(mock_sysinfo_stream_stop).toHaveBeenCalledTimes(1);
+    });
   });
 });
 //===========================================================================
--- src/middlewares/sysinfo/sysinfo.js
+++ src/middlewares/sysinfo/sysinfo.js
@@ -4,7 +4,8 @@
 const sysinfoModule = require('../../modules/sysinfo');
 
 const initialize = (sse_handler) => {
-  const sysinfo = sysinfoModule.initialize((data) => sse_handler?.send('sysinfo', data));
+  const sysinfo = sysinfoModule.initialize();
+  sysinfo.stream.subscribe((data) => sse_handler?.send('sysinfo', data));
 
   sse_handler?.onClientCountChange('sysinfo', (client_count) => {
     (client_count > 0) ? sysinfo.stream.start() : sysinfo.stream.stop();
